b802a2cdec41d45b65ef951f2dd7bf4d6df3f83a
kmattimo <kmattimo@umich.edu>
2014-12-05 16:11:30 -0500

    favicon

---
+++ templates/templates_c/890ef62e025420ec0fe485ff5ec34468731b12aa.file.index.tpl.php
@@ -0,0 +1,65 @@
+<?php /* Smarty version Smarty-3.1.14, created on 2014-12-05 22:02:37
+         compiled from "C:\Users\kyle\Dropbox\Documents\Code\GitHub\kylemattimore.com\templates\templates\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1565554821d6d6b54d4-30823426%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '890ef62e025420ec0fe485ff5ec34468731b12aa' => 
+    array (
+      0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\index.tpl',
+      1 => 1416949297,
+      2 => 'file',
+    ),
+    '5ac0f611db21789fd7234be4426b5552847f7007' => 
+    array (
+      0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\base.tpl',
+      1 => 1417758518,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1565554821d6d6b54d4-30823426',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_54821d6da99657_83058431',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54821d6da99657_83058431')) {function content_54821d6da99657_83058431($_smarty_tpl) {?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+<head>
+	
+		<!-- Bootstrap -->
+    <link href="static/css/bootstrap.min.css" rel="stylesheet">
+	
+    <link rel="stylesheet" href="/static/css/style.css" />
+
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<body>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="static/js/bootstrap.min.js"></script>
+
+  <div class="center">
+    
+<h1>INDEX</h1>
+<p class="center">
+  Welcome!
+</p>
+
+  </div>
+  <script type="text/javascript" src="/static/js/main.js"></script>
+</body>
+
+</html><?php }} ?>
