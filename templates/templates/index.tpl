36b866e645a491be9be7c07117328564c2e7e22a
kmattimo <kmattimo@umich.edu>
2014-12-05 21:11:56 -0500

    nonsense

---
+++ templates/templates/index.tpl
@@ -0,0 +1,61 @@
+{* Smarty *}
+{extends 'base.tpl'}
+{block name='body'}
+
+
+	<div class="jumbotron text-center">
+		<h1> Kyle Mattimore </h1>
+		
+			<div class="container" >
+				<ul class="nav nav-justified" role="navigation">
+					<li><a href="#">Home</a></li>
+					{*<li><a href="#">About</a></li>*}
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+	</div> 
+
+
+<div class="container">
+		<div class="row">
+				<div class="col-sm-3">
+					<div class="well well-sm">
+						<ul class="nav nav-stacked">
+							<li><a href="#">Facebook</a></li>
+							<li><a href="#">LinkedIn</a></li>
+							<li><a href="#">GitHub</a></li>
+							<li><a href="#">YouTube</a></li>
+							<li><a href="#">Blog</a></li>
+			
+						</ul>
+					</div>
+				</div> 
+
+				<div class="col-sm-9">
+					<div class="row ">
+						<div class="col-xs-4">
+							<img src="static/images/test1.jpg" class="img-responsive img-rounded " alt="...">
+							 
+						</div>
+						<div class="col-xs-5">
+							<img src="static/images/test2.jpg" class="img-responsive img-rounded" alt="...">
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class "col-xs-12">
+							<div class="well ">Lorem ipsum dolor sit amet, posse semper an eam. Et iriure abhorreant cum, ad autem altera diceret eam. In vim brute soleat tamquam, vivendo adversarium mea ne, sea reque pericula constituto te. Qui et mutat laoreet, vitae feugait nominati pri ex. Et atqui facete vis, in eam utinam facilisis, integre dolorum est et. </div>
+						</div>
+					</div>
+		
+				</div>
+		
+		</div>
+</div>
+	
+
+	
+	
+
+{/block}
--- templates/templates_c/890ef62e025420ec0fe485ff5ec34468731b12aa.file.index.tpl.php
+++ templates/templates_c/890ef62e025420ec0fe485ff5ec34468731b12aa.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2014-12-06 02:54:51
+<?php /* Smarty version Smarty-3.1.14, created on 2014-12-06 03:09:12
          compiled from "C:\Users\kyle\Dropbox\Documents\Code\GitHub\kylemattimore.com\templates\templates\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2492754825b46c407f8-45563247%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '890ef62e025420ec0fe485ff5ec34468731b12aa' => 
     array (
       0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\index.tpl',
-      1 => 1417830507,
+      1 => 1417831696,
+      2 => 'file',
+    ),
+    '5ac0f611db21789fd7234be4426b5552847f7007' => 
+    array (
+      0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\base.tpl',
+      1 => 1417815886,
       2 => 'file',
     ),
   ),
@@ -17,6 +23,105 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'version' => 'Smarty-3.1.14',
   'unifunc' => 'content_54825b46cb4835_57928280',
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_54825b46cb4835_57928280')) {function content_54825b46cb4835_57928280($_smarty_tpl) {?><?php }} ?>
+<?php if ($_valid && !is_callable('content_54825b46cb4835_57928280')) {function content_54825b46cb4835_57928280($_smarty_tpl) {?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+<head>
+	
+		<!-- Bootstrap -->
+    <link href="static/css/bootstrap.min.css" rel="stylesheet">
+	
+    <link rel="stylesheet" href="/static/css/style.css" />
+
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</title>
+</head>
+<body>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="static/js/bootstrap.min.js"></script>
+
+	
+	
+	
+	
+	
+  <div>
+    
+
+
+	<div class="jumbotron text-center">
+		<h1> Kyle Mattimore </h1>
+		
+			<div class="container" >
+				<ul class="nav nav-justified" role="navigation">
+					<li><a href="#">Home</a></li>
+					
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+	</div> 
+
+
+<div class="container">	
+		<div class="row">
+				<div class="col-sm-3">
+					<div class="well well-sm">
+						<ul class="nav nav-stacked">
+							<li><a href="#">Facebook</a></li>
+							<li><a href="#">LinkedIn</a></li>
+							<li><a href="#">GitHub</a></li>
+							<li><a href="#">YouTube</a></li>
+							<li><a href="#">Blog</a></li>
+			
+						</ul>
+					</div>
+				</div> 
+
+				<div class="col-sm-9">
+					<div class="row ">
+						<div class="col-xs-4">
+							<img src="static/images/test1.jpg" class="img-responsive img-rounded " alt="...">
+							 
+						</div>
+						<div class="col-xs-5">
+							<img src="static/images/test2.jpg" class="img-responsive img-rounded" alt="...">
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class "col-xs-12">
+							<div class="well ">Lorem ipsum dolor sit amet, posse semper an eam. Et iriure abhorreant cum, ad autem altera diceret eam. In vim brute soleat tamquam, vivendo adversarium mea ne, sea reque pericula constituto te. Qui et mutat laoreet, vitae feugait nominati pri ex. Et atqui facete vis, in eam utinam facilisis, integre dolorum est et. </div>
+						</div>
+					</div>
+		
+				</div>
+			
+			
+			
+		
+		</div>
+	
+	
+	</div>
+	
+
+	
+	
+
+
+  </div>
+  <script type="text/javascript" src="/static/js/main.js"></script>
+</body>
+
+</html><?php }} ?>
