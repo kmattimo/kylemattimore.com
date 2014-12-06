245493d1e0fc8c70d1fbc3be14e0cc3d8b9640c4
kmattimo <kmattimo@umich.edu>
2014-12-05 20:57:57 -0500

    moar layout

--- templates/templates_c/890ef62e025420ec0fe485ff5ec34468731b12aa.file.index.tpl.php
+++ templates/templates_c/890ef62e025420ec0fe485ff5ec34468731b12aa.file.index.tpl.php
@@ -1,111 +1,22 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2014-12-06 01:54:50
+<?php /* Smarty version Smarty-3.1.14, created on 2014-12-06 02:54:51
          compiled from "C:\Users\kyle\Dropbox\Documents\Code\GitHub\kylemattimore.com\templates\templates\index.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:1290054824e89d496c2-02880111%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:2492754825b46c407f8-45563247%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '890ef62e025420ec0fe485ff5ec34468731b12aa' => 
     array (
       0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\index.tpl',
-      1 => 1417827288,
-      2 => 'file',
-    ),
-    '5ac0f611db21789fd7234be4426b5552847f7007' => 
-    array (
-      0 => 'C:\\Users\\kyle\\Dropbox\\Documents\\Code\\GitHub\\kylemattimore.com\\templates\\templates\\base.tpl',
-      1 => 1417815886,
+      1 => 1417830507,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '1290054824e89d496c2-02880111',
+  'nocache_hash' => '2492754825b46c407f8-45563247',
   'function' => 
   array (
   ),
   'version' => 'Smarty-3.1.14',
-  'unifunc' => 'content_54824e89db7e09_64107546',
-  'variables' => 
-  array (
-    'title' => 0,
-  ),
+  'unifunc' => 'content_54825b46cb4835_57928280',
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_54824e89db7e09_64107546')) {function content_54824e89db7e09_64107546($_smarty_tpl) {?>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html lang="en">
-<head>
-	
-		<!-- Bootstrap -->
-    <link href="static/css/bootstrap.min.css" rel="stylesheet">
-	
-    <link rel="stylesheet" href="/static/css/style.css" />
-
-    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
-</title>
-</head>
-<body>
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="static/js/bootstrap.min.js"></script>
-
-	
-	
-	
-	
-	
-  <div>
-    
-
-
-
-	<div class="jumbotron text-center">
-		<h1> Kyle Mattimore </h1>
-		
-			<div class="container" >
-				<ul class="nav nav-justified" role="navigation">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Projects</a></li>
-					<li><a href="#">Contact</a></li>
-				</ul>
-			</div>
-	</div> 
-
-	
-<div class="container">	
-		<div class="row">
-				<div class="col-sm-3">
-					
-					<ul class="nav nav-stacked">
-						<li><a href="#">Facebook</a></li>
-						<li><a href="#">LinkedIn</a></li>
-						<li><a href="#">YouTube</a></li>
-						<li><a href="#">etc</a></li>
-					</ul>
-					
-				</div> 
-
-				<div class="col-sm-9">
-					<div class="well"> Big column
-						</div>
-				
-				</div>
-			
-		
-		</div>
-	
-	
-	</div>
-	
-
-	
-	
-
-
-  </div>
-  <script type="text/javascript" src="/static/js/main.js"></script>
-</body>
-
-</html><?php }} ?>
+<?php if ($_valid && !is_callable('content_54825b46cb4835_57928280')) {function content_54825b46cb4835_57928280($_smarty_tpl) {?><?php }} ?>
